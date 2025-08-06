d9b745e630f6d99875f2331ba1286a8e0726de78
tiaggy <110980386+tiaggy@users.noreply.github.com>
2025-08-06 22:42:47 +0200

    Initial commit

---
+++ src/main/java/me/tiaggy/dresscode/utils/AppearanceTypeAdapter.java
@@ -0,0 +1,33 @@
+package me.tiaggy.dresscode.utils;
+
+import com.google.gson.*;
+import me.tiaggy.dresscode.models.PlayerData;
+
+import java.lang.reflect.Type;
+
+public class AppearanceTypeAdapter implements JsonSerializer<PlayerData>, JsonDeserializer<PlayerData> {
+
+    @Override
+    public JsonElement serialize(PlayerData src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("basicSkin", src.getBasicSkin());
+        jsonObject.addProperty("playerState", src.getPlayerState());
+        jsonObject.addProperty("hat", src.getHat());
+        jsonObject.addProperty("chest", src.getChest());
+        jsonObject.addProperty("trousers", src.getTrousers());
+        jsonObject.addProperty("boots", src.getBoots());
+        return jsonObject;
+    }
+
+    @Override
+    public PlayerData deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        String basicSkin = jsonObject.get("basicSkin").getAsString();
+        String playerState = jsonObject.get("playerState").getAsString();
+        String hat = jsonObject.get("hat").getAsString();
+        String chest = jsonObject.get("chest").getAsString();
+        String trousers = jsonObject.get("trousers").getAsString();
+        String boots = jsonObject.get("boots").getAsString();
+        return new PlayerData(basicSkin, playerState, hat, chest, trousers, boots);
+    }
+}
